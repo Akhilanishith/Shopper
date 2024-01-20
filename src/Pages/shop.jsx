731c3f04483d2d5952360bd0188217d23e9f3aa9
Akhila <nikkimaipaje@gmail.com>
2024-01-20 10:49:28 +0530

    first commit

---
+++ src/Pages/shop.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import Popular from "../components/Popular/Popular";
+// import Item from '../components/Item/item';
+
+
+
+
+const Shop = () => {
+    return (
+        <div>
+          <Hero/>
+          <Popular/>
+        
+       
+          
+        </div>
+    );
+};
+export default Shop;
